aded277b2f7921e9570e263ed88f2c67f52be0f1
IT21307058 <IT21307058@my.sliit.lk>
2024-07-04 18:31:27 +0530

    add model class and repositry interface

---
+++ src/main/java/com/taskmanagementsystem/model/Task.java
@@ -0,0 +1,27 @@
+package com.taskmanagementsystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="task")
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    private String description;
+
+    private String status;
+
+}
